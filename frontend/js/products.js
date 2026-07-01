cc2983bebce7f91c6d595d954d30b5ab029444a5
NguyenHoangTuan7 <tuannh9930@ut.edu.vn>
2026-07-01 10:41:17 +0700

    Update products.js

--- frontend/js/products.js
+++ frontend/js/products.js
@@ -293,3 +293,20 @@ function changeTitle() {
     }
 
 }
+function updateCartCount(){
+
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const count = cart.reduce((sum,item)=>sum + item.quantity,0);
+
+    const cartCount = document.getElementById("cart-count");
+
+    if(cartCount){
+
+        cartCount.innerText = count;
+
+    }
+
+}
+
+updateCartCount();
